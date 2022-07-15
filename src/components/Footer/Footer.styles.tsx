d10c284691243a17c296dd0356a3d86f047f35a2
André Canton <andrecanton18@gmail.com>
2022-07-14 21:47:36 -0300

    feat: add Footer component

---
+++ src/components/Footer/Footer.styles.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 20px 0;
+    background-color: #202432;
+`
+
+export const CopyrightText = styled.span`
+    font-size: 12px;
+    color: white;
+`
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FooterContainer, CopyrightText } from "./Footer.styles";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <CopyrightText>Copyright © 2022 Sencon</CopyrightText>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
